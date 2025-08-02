fd089d41e49eebd9ae8da2d7e00c5502f8fd3e68
Pratyush439 <156572277+Pratyush439@users.noreply.github.com>
2025-08-02 12:01:04 +0530

    Delete sceneapp directory

--- sceneapp/android/app/src/main/kotlin/com/example/sceneapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sceneapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- sceneapp/lib/ip_address.dart
+++
@@ -1,3 +0,0 @@
-class Config {
-  static const String IP_ADDRESS = "192.168.29.240"; // For emulator, put 127.0.0.1
-}
